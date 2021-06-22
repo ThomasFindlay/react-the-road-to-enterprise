c6aad549064b0540af6413c438c25e6e4ddadd49
Thomas Findlay <thomasfindlay94@gmail.com>
2021-06-22 13:09:59 +0100

    base setup

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  endOfLine: 'lf',
+  jsxBracketSameLine: false,
+  printWidth: 80,
+  proseWrap: 'never',
+  quoteProps: 'as-needed',
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
+};
