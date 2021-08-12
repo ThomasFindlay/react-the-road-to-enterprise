309f67a48f86af694f2ffb887d7915ea7149ee48
Thomas <thomasfindlay94@gmail.com>
2021-08-12 13:13:40 +0100

    Configured src alias

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,21 @@
 const postcssConfig = require('./postcss.config')
-
+const cracoAlias = require('craco-alias')
 module.exports = {
   style: {
     postcss: postcssConfig,
   },
+  plugins: [
+    {
+      plugin: cracoAlias,
+      options: {
+        source: 'tsconfig',
+        // baseUrl SHOULD be specified
+        // plugin does not take it from tsconfig
+        baseUrl: './',
+        /* tsConfigPath should point to the file where "baseUrl" and "paths" 
+          are specified*/
+        tsConfigPath: './tsconfig.paths.json',
+      },
+    },
+  ],
 }
