21c561eb16055a465a8652c31c17c1f215c92d81
Thomas <thomasfindlay94@gmail.com>
2021-08-30 08:41:54 +0100

    Updated the main h1 and stylelint to ignore node_modules

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import AnimalExample from '@/components/AnimalExample'
 function App() {
   return (
     <div className="App mx-auto max-w-6xl text-center my-8">
-      <h1>React - The Road To Enterprise</h1>
+      <h1 className="font-semibold text-2xl">React - The Road To Enterprise</h1>
       <AnimalExample />
     </div>
   )
