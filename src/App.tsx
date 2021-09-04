4623bf214d394f598017120143c1d5851595d4d2
Thomas <thomasfindlay94@gmail.com>
2021-09-04 19:54:43 +0100

    Fresh start

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import { QueryClient, QueryClientProvider } from 'react-query'
 import './App.css'
-import FetchTopQuotes from '@/components/FetchTopQuotes'
-import UpdateQuotes from '@/components/UpdateQuotes'
-import PaginatedQuotes from '@/components/PaginatedQuotes'
-import InfiniteScrollQuotes from '@/components/InfiniteScrollQuotes'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import QueryCancellation from './components/QueryCancellation'
 const queryClient = new QueryClient()
 
 function App() {
@@ -18,11 +13,6 @@ function App() {
           <h1 className="font-semibold text-2xl">
             React - The Road To Enterprise
           </h1>
-          <UpdateQuotes />
-          <FetchTopQuotes />
-          <PaginatedQuotes />
-          <InfiniteScrollQuotes />
-          <QueryCancellation />
         </div>
       </QueryClientProvider>
     </>
