b8a195316f184a75020b99e8e33537acf9365969
Thomas <thomasfindlay94@gmail.com>
2021-09-08 11:32:12 +0100

    Added context factory

---
+++ src/context/helpers/contextFactory.tsx
@@ -0,0 +1,39 @@
+import {
+  createContext,
+  useContext,
+  useContextSelector,
+} from 'use-context-selector'
+
+export const contextFactory = <CtxState,>() => {
+  const context = createContext<CtxState | undefined>(undefined)
+
+  const useCtx = () => {
+    const ctx = useContext(context)
+    if (ctx === undefined)
+      throw new Error(
+        'useContextSelector must be used within a context provider'
+      )
+
+    return ctx
+  }
+
+  type ContextSelector<Selected, CtxState> = (ctxState: CtxState) => Selected
+
+  const useCtxSelector = <Selected,>(
+    contextSelector: ContextSelector<Selected, CtxState>
+  ) => {
+    const selector = (state: CtxState | undefined) => {
+      if (state === undefined)
+        throw new Error('useContext must be used within a context provider')
+
+      return contextSelector(state)
+    }
+
+    return useContextSelector<
+      CtxState | undefined,
+      ReturnType<typeof selector>
+    >(context, selector)
+  }
+
+  return [context, useCtx, useCtxSelector] as const
+}
