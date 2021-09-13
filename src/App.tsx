a699ef00cee416f9bfe6aa6da6b8fdf57f833c5d
Thomas <thomasfindlay94@gmail.com>
2021-09-13 07:46:11 +0100

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import './App.css'
-import AnimalExample from '@/components/AnimalExample'
 function App() {
   return (
     <div className="App mx-auto max-w-6xl text-center my-8">
       <h1 className="font-semibold text-2xl">React - The Road To Enterprise</h1>
-      <AnimalExample />
     </div>
   )
 }
