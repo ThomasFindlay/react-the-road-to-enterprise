ba99f6e287bf22e442af3db2f2561645cab008c5
Thomas <thomasfindlay94@gmail.com>
2021-10-25 15:24:22 +0100

    Merge conflict fix

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import usersReducer from '@/components/UsersManager/usersSlice'
+<<<<<<< HEAD
 import { useDispatch, useSelector } from 'react-redux'
 
+=======
+>>>>>>> dc81bcb (Removed comments from the store/index.ts file)
 export const store = configureStore({
   reducer: {
     users: usersReducer,
   },
 })
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
