d1e909b044d1465f853f58055efeb593e135875f
Thomas <thomasfindlay94@gmail.com>
2022-01-10 11:24:50 +0000

    Deleted server folder

--- server/node_modules/@fastify/ajv-compiler/test/types/index.test.ts
+++
@@ -1,6 +0,0 @@
-import { expectType } from "tsd";
-import ValidatorSelector, { ValidatorCompiler } from "../..";
-
-const compiler = ValidatorSelector();
-
-expectType<ValidatorCompiler>(compiler);
--- server/node_modules/avvio/test/fixtures/plugin-no-next.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-module.exports = function (app, opts, next) {
-  // no call to next
-}
