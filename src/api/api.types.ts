1a691456f656867187466b5fef1bc4f462f36b76
Thomas <thomasfindlay94@gmail.com>
2022-01-18 08:47:28 +0000

    Added API layer

---
+++ src/api/api.types.ts
@@ -0,0 +1,31 @@
+import {
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosError,
+  AxiosPromise,
+  Canceler,
+} from 'axios'
+
+export type { Canceler }
+
+type AxiosMethods = Pick<
+  AxiosInstance,
+  'get' | 'put' | 'patch' | 'post' | 'delete'
+>
+export type WithAbordFn = AxiosMethods[keyof AxiosMethods]
+
+export type ApiExecutor<T> = {
+  (url: string, body: unknown, config: ApiRequestConfig): AxiosPromise<T>
+  (url: string, config: ApiRequestConfig): AxiosPromise<T>
+}
+export type ApiExecutorArgs =
+  | [string, unknown, ApiRequestConfig]
+  | [string, ApiRequestConfig]
+
+export type ApiRequestConfig = AxiosRequestConfig & {
+  abort?: (cancel: Canceler) => void
+}
+
+export type ApiError = AxiosError & {
+  aborted?: boolean
+}
