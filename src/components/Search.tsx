b5d54ceff248e937ac09b45936feedaf3d52e318
Thomas <thomasfindlay94@gmail.com>
2022-01-18 09:41:17 +0000

    Added API request to fetch meals and render them

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,30 +1,42 @@
+import { fetchMeals, Meal } from '@/api/mealApi'
 import { debounce } from '@/helpers/debounce'
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
 type SearchProps = {}
 
-const initSearchApiRequest = debounce((q: string) => {
-  console.log('INITIALISE API REQUEST')
-}, 500)
-
 const Search = (props: SearchProps) => {
   const [query, setQuery] = useState('')
+  const [meals, setMeals] = useState<Meal[]>([])
+
+  const initSearchApiRequest = useMemo(() => {
+    return debounce(async (q: string) => {
+      setMeals(await fetchMeals(q))
+    }, 500)
+  }, [])
 
   const onChangeQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
     const q = e.target.value
     setQuery(q)
     initSearchApiRequest(q)
   }
+
   return (
-    <form className="flex flex-col items-start mx-auto max-w-[20rem]">
-      <label>Search</label>
-      <input
-        className="w-full"
-        type="text"
-        value={query}
-        onChange={onChangeQuery}
-      />
-    </form>
+    <div className="space-y-6">
+      <form className="flex flex-col items-start mx-auto max-w-[20rem]">
+        <label>Search meals</label>
+        <input
+          className="w-full"
+          type="text"
+          value={query}
+          onChange={onChangeQuery}
+        />
+      </form>
+      <ul>
+        {meals.map((meal) => {
+          return <li key={meal.idMeal}>{meal.strMeal}</li>
+        })}
+      </ul>
+    </div>
   )
 }
 
