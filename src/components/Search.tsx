1df7d21905ccf5de1d89cfbd882e52198e09b655
Thomas <thomasfindlay94@gmail.com>
2022-01-18 09:44:24 +0000

    Initial setup

---
+++ src/components/Search.tsx
@@ -0,0 +1,40 @@
+import { fetchMeals, Meal } from '@/api/mealApi'
+import React, { useState } from 'react'
+
+type SearchProps = {}
+
+const Search = (props: SearchProps) => {
+  const [query, setQuery] = useState('')
+  const [meals, setMeals] = useState<Meal[]>([])
+
+  const initSearchApiRequest = async (q: string) => {
+    setMeals(await fetchMeals(q))
+  }
+
+  const onChangeQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const q = e.target.value
+    setQuery(q)
+    initSearchApiRequest(q)
+  }
+
+  return (
+    <div className="space-y-6">
+      <form className="flex flex-col items-start mx-auto max-w-[20rem]">
+        <label>Search meals</label>
+        <input
+          className="w-full"
+          type="text"
+          value={query}
+          onChange={onChangeQuery}
+        />
+      </form>
+      <ul>
+        {meals.map((meal) => {
+          return <li key={meal.idMeal}>{meal.strMeal}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Search
