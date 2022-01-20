6aa828fcd9c5a12cd74917cca20e262ef3fd16ec
Thomas <thomasfindlay94@gmail.com>
2022-01-20 09:52:55 +0000

    Removed unnecessary div

--- src/components/ingredients/IngredientsInfoHelper.tsx
+++ src/components/ingredients/IngredientsInfoHelper.tsx
@@ -3,11 +3,9 @@ type IngredientsInfoHelperProps = {}
 const IngredientsInfoHelper = (props: IngredientsInfoHelperProps) => {
   console.log('IngredientsInfoHelper rendered')
   return (
-    <div>
-      <button className="w-5 h-5 rounded-full bg-blue-100 hover:bg-blue-200 flex items-center justify-center">
-        &#33;
-      </button>
-    </div>
+    <button className="w-5 h-5 rounded-full bg-blue-100 hover:bg-blue-200 flex items-center justify-center">
+      &#33;
+    </button>
   )
 }
 
