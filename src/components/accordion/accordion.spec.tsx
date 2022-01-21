797d02ed41669a21c9b05528bc36a4d5727f7f6a
Thomas <thomasfindlay94@gmail.com>
2022-01-21 09:51:08 +0000

    Tests setup

--- src/components/accordion/accordion.spec.tsx
+++ src/components/accordion/accordion.spec.tsx
@@ -20,28 +20,9 @@ const renderAccordion = (props = {}) =>
   render(<Accordion items={accordionData} {...props} />)
 
 describe('Accordion.tsx', () => {
-  it('Accordion items have correct text', async () => {
-    renderAccordion()
-    // Match Text content
-    accordionData.forEach((data) => {
-      expect(screen.getByText(data.heading)).toHaveTextContent(data.heading)
-      expect(screen.getByText(data.content)).toHaveTextContent(data.content)
-    })
-  })
+  it('Accordion items have correct text', async () => {})
 
-  it('Each accordion item content is hidden at the start', async () => {
-    renderAccordion()
-    screen.getAllByTestId('accordion-item-content').forEach((el) => {
-      expect(el).toHaveClass('hidden')
-    })
-  })
+  it('Each accordion item content is hidden at the start', async () => {})
 
-  it('Accordion item content is toggled on header click', async () => {
-    renderAccordion()
-    const heading = accordionData[1].heading
-    const content = accordionData[1].content
-    expect(screen.getByText(content)).toHaveClass('hidden')
-    fireEvent.click(screen.getByText(heading))
-    expect(screen.getByText(content)).toHaveClass('block')
-  })
+  it('Accordion item content is toggled on header click', async () => {})
 })
