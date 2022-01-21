08073b3df96f40900ac0d70d39de3ff3cb64b49d
Thomas <thomasfindlay94@gmail.com>
2022-01-21 09:56:24 +0000

    Style updates

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,8 @@ function App() {
   return (
     <div className="App mx-auto max-w-6xl text-center my-8">
       <h1 className="font-semibold text-2xl">React - The Road To Enterprise</h1>
-      <div className="mt-8">
+      <div className="mt-8 max-w-[20rem] mx-auto">
+        <h2 className="mb-4 text-lg font-semibold">Accordion</h2>
         <Accordion items={items} />
       </div>
     </div>
