26f2719386da00cdf83fa8460038f4ae7544e405
Thomas <thomasfindlay94@gmail.com>
2022-01-24 15:45:35 +0000

    Text update

--- src/components/newsletterForm.spec.tsx
+++ src/components/newsletterForm.spec.tsx
@@ -18,7 +18,7 @@ const renderNewsletterForm = () => {
 describe('NewsletterForm.tsx', () => {
   it('Should show an error message if submitting without filling in all the fields.', async () => {})
 
-  it('Should show a success message when the users joins the newsletter', async () => {})
+  it('Should show a success message when the user joins the newsletter', async () => {})
 
   it('Should show an error if the request to join the newsletter fails.', async () => {})
 })
