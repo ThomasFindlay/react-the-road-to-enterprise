cc962994ce8a3495c8270835279f33a8edd79450
Thomas <thomasfindlay94@gmail.com>
2022-01-24 09:13:54 +0000

    Newsletter form setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
 import './App.css'
-import Accordion from './components/accordion/Accordion'
-
-const items = [
-  {
-    heading: 'Heading one',
-    content: 'Content one',
-  },
-  {
-    heading: 'Heading two',
-    content: 'Content two',
-  },
-  {
-    heading: 'Heading three',
-    content: 'Content three',
-  },
-]
+import NewsletterForm from './components/NewsletterForm'
 
 function App() {
   return (
     <div className="App mx-auto max-w-6xl text-center my-8">
       <h1 className="font-semibold text-2xl">React - The Road To Enterprise</h1>
       <div className="mt-8 max-w-[20rem] mx-auto">
-        <h2 className="mb-4 text-lg font-semibold">Accordion</h2>
-        <Accordion items={items} />
+        <NewsletterForm />
       </div>
     </div>
   )
