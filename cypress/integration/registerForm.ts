c57ccaf5de66124a1c7a44cb13aa59d21674720b
Thomas <thomasfindlay94@gmail.com>
2022-01-28 19:39:53 +0000

    Added UserData type

--- cypress/integration/registerForm.ts
+++ cypress/integration/registerForm.ts
@@ -1,6 +1,15 @@
 // Helper to go to the next step
 const goNext = () => cy.findByText('Next').click()
 
+type UserData = {
+  name: string
+  surname: string
+  address: string
+  city: string
+  email: string
+  password: string
+}
+
 describe('User Registration', () => {
   beforeEach(() => {})
 
