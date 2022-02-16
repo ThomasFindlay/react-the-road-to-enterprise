dc654aedd206d381b0ac45aed52b82055e6bf4df
Thomas <thomasfindlay94@gmail.com>
2022-02-16 13:07:31 +0000

    Conditional label

--- src/components/wrapper/DatePicker.tsx
+++ src/components/wrapper/DatePicker.tsx
@@ -1,14 +1,14 @@
 import ReactDatePicker, { ReactDatePickerProps } from 'react-datepicker'
 
 type DatePickerProps = {
-  label: string
+  label?: string
 } & ReactDatePickerProps
 
 const DatePicker = (props: DatePickerProps) => {
   const { label, ...datePickerProps } = props
   return (
     <div className="flex flex-col space-y-2">
-      <label>{label}</label>
+      {label ? <label>{label}</label> : null}
       <ReactDatePicker {...datePickerProps} />
     </div>
   )
