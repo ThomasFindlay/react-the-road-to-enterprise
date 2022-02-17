118caf02d7046a8957ade3620bb7313131381f69
Thomas <thomasfindlay94@gmail.com>
2022-02-17 13:10:32 +0000

    Added CloseIcon to barrel exports

--- src/components/composition-configuration/components/icons/index.tsx
+++ src/components/composition-configuration/components/icons/index.tsx
@@ -1,3 +1,4 @@
 export { default as SuccessIcon } from './SuccessIcon'
 export { default as ErrorIcon } from './ErrorIcon'
 export { default as InfoIcon } from './InfoIcon'
+export { default as CloseIcon } from './CloseIcon'
