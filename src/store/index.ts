f0bbd2a665fdd6933ff05d43b7d88c1a6b45b81a
Thomas <thomasfindlay94@gmail.com>
2022-03-13 22:44:25 +0000

    merged

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
 import usersReducer from '@/components/UsersManager/usersSlice'
-<<<<<<< HEAD
-import { useDispatch, useSelector } from 'react-redux'
-
-=======
->>>>>>> dc81bcb (Removed comments from the store/index.ts file)
 export const store = configureStore({
   reducer: {
     users: usersReducer,
