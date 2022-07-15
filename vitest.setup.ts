f8ddd808bbc81b9dd37bf69d3b0d06b2708ba0be
Thomas <thomasfindlay94@gmail.com>
2022-07-15 12:32:22 +0100

    Added Tailwind and whatwg-fetch polyfill to the vitest setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,5 +1,6 @@
 // Include any code that should run before tests
 import matchers from '@testing-library/jest-dom/matchers';
 import { expect } from 'vitest';
+import 'whatwg-fetch';
 
 expect.extend(matchers);
