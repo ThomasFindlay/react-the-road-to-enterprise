103d296c474bdf245e20a3a37f7711ee0e44f78b
Thomas <thomasfindlay94@gmail.com>
2022-07-15 11:25:40 +0100

    Prettier fix

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -1,41 +1,41 @@
-import clsx from 'clsx'
-import { useState } from 'react'
+import clsx from 'clsx';
+import { useState } from 'react';
 interface Props {
   items: {
-    heading: string
-    content: string
-  }[]
+    heading: string;
+    content: string;
+  }[];
 }
 
-type OpenIndexes = Record<string, boolean>
+type OpenIndexes = Record<string, boolean>;
 
 const Accordion = (props: Props) => {
-  const { items } = props
-  const [openIndexes, setOpenIndexes] = useState<OpenIndexes>({})
+  const { items } = props;
+  const [openIndexes, setOpenIndexes] = useState<OpenIndexes>({});
 
   const onAccordionItemHeaderClick = (index: number) => {
     setOpenIndexes((state) => ({
       ...state,
       [index]: !state[index],
-    }))
-  }
+    }));
+  };
 
   return (
-    <div data-testid="accordion">
+    <div data-testid='accordion'>
       {items.map((item, index) => {
         return (
           <div
             key={index}
             className={clsx('border', index < items.length - 1 && 'border-b-0')}
-            data-testid="accordion-item"
+            data-testid='accordion-item'
           >
             <button
               id={`acc-header-${index}`}
-              className="px-4 py-3 block w-full cursor-pointer bg-gray-100 hover:bg-gray-200"
+              className='px-4 py-3 block w-full cursor-pointer bg-gray-100 hover:bg-gray-200'
               onClick={() => onAccordionItemHeaderClick(index)}
               aria-controls={`acc-item-${index}`}
               aria-expanded={!!openIndexes[index]}
-              data-testid="accordion-item-header"
+              data-testid='accordion-item-header'
             >
               {item.heading}
             </button>
@@ -46,15 +46,15 @@ const Accordion = (props: Props) => {
                 openIndexes[index] ? 'block' : 'hidden'
               )}
               aria-labelledby={`acc-header-${index}`}
-              data-testid="accordion-item-content"
+              data-testid='accordion-item-content'
             >
               {item.content}
             </div>
           </div>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default Accordion
+export default Accordion;
