470241ad9787ff0c92f569226f2a1114a5f4725e
Thomas <thomasfindlay94@gmail.com>
2022-07-15 12:00:38 +0100

    Converted the codebase from CRA to Vite
    Cleanup and updated test-utils types
    
    Added disable next line for props

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import './App.css'
-import NewsletterForm from './components/NewsletterForm'
+import './App.css';
+import NewsletterForm from './components/NewsletterForm';
 
 function App() {
   return (
-    <div className="App mx-auto max-w-6xl text-center my-8">
-      <h1 className="font-semibold text-2xl">React - The Road To Enterprise</h1>
-      <div className="mt-8 max-w-[20rem] mx-auto">
+    <div className='App mx-auto max-w-6xl text-center my-8'>
+      <h1 className='font-semibold text-2xl'>React - The Road To Enterprise</h1>
+      <div className='mt-8 max-w-[20rem] mx-auto'>
         <NewsletterForm />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NewsletterForm.tsx
+++ src/components/NewsletterForm.tsx
@@ -1,101 +1,103 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-type NewsletterFormProps = {}
+// eslint-disable-next-line @typescript-eslint/ban-types
+type NewsletterFormProps = {};
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const NewsletterForm = (props: NewsletterFormProps) => {
   const [form, setForm] = useState({
     name: '',
     email: '',
-  })
+  });
   const [joinNewsletterApiStatus, setJoinNewsletterApiStatus] = useState<
     'IDLE' | 'PENDING' | 'SUCCESS' | 'ERROR'
-  >('IDLE')
-  const [error, setError] = useState('')
+  >('IDLE');
+  const [error, setError] = useState('');
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
+    const { name, value } = e.target;
     setForm((state) => ({
       ...state,
       [name]: value,
-    }))
-    error && setError('')
-  }
+    }));
+    error && setError('');
+  };
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    if (joinNewsletterApiStatus === 'PENDING') return
-    setJoinNewsletterApiStatus('PENDING')
+    e.preventDefault();
+    if (joinNewsletterApiStatus === 'PENDING') return;
+    setJoinNewsletterApiStatus('PENDING');
     if (!form.name || !form.email) {
-      setError('Please fill in all the fields.')
-      return
+      setError('Please fill in all the fields.');
+      return;
     }
     try {
       const response = await fetch('https://myserver.com/api/newsletter/join', {
         method: 'POST',
         body: JSON.stringify(form),
-      })
+      });
 
       if (response.ok) {
-        setJoinNewsletterApiStatus('SUCCESS')
-        return
+        setJoinNewsletterApiStatus('SUCCESS');
+        return;
       }
       throw new Error(
         'There was a problem while signing you up for the newsletter. Please try again.'
-      )
+      );
     } catch (error) {
       setError(
         'There was a problem while signing you up for the newsletter. Please try again.'
-      )
-      setJoinNewsletterApiStatus('ERROR')
+      );
+      setJoinNewsletterApiStatus('ERROR');
     }
-  }
+  };
 
   return (
     <div>
-      <h2 className="mb-6 text-xl font-semibold">Sign up for our newsletter</h2>
+      <h2 className='mb-6 text-xl font-semibold'>Sign up for our newsletter</h2>
       {joinNewsletterApiStatus === 'SUCCESS' ? (
         <div>You have joined the newsletter!</div>
       ) : (
         <form onSubmit={onSubmit}>
-          <fieldset className="mb-4 flex flex-col space-y-2 items-start">
-            <label htmlFor="name">Name</label>
+          <fieldset className='mb-4 flex flex-col space-y-2 items-start'>
+            <label htmlFor='name'>Name</label>
             <input
-              className="w-full"
-              type="text"
-              id="name"
-              name="name"
+              className='w-full'
+              type='text'
+              id='name'
+              name='name'
               value={form.name}
               onChange={onChange}
             />
           </fieldset>
-          <fieldset className="mb-4 flex flex-col space-y-2 items-start">
-            <label htmlFor="email">Email</label>
+          <fieldset className='mb-4 flex flex-col space-y-2 items-start'>
+            <label htmlFor='email'>Email</label>
             <input
-              className="w-full"
-              type="email"
-              id="email"
-              name="email"
+              className='w-full'
+              type='email'
+              id='email'
+              name='email'
               value={form.email}
               onChange={onChange}
-              data-testid="emailInput"
+              data-testid='emailInput'
             />
           </fieldset>
 
           {joinNewsletterApiStatus === 'ERROR' || error ? (
-            <p className="text-red-700 mb-4">{error}</p>
+            <p className='text-red-700 mb-4'>{error}</p>
           ) : null}
 
           <button
-            className="px-4 py-3 bg-blue-700 text-blue-100 min-w-[5rem] font-semibold "
+            className='px-4 py-3 bg-blue-700 text-blue-100 min-w-[5rem] font-semibold '
             disabled={joinNewsletterApiStatus === 'PENDING'}
-            type="submit"
+            type='submit'
           >
             {joinNewsletterApiStatus === 'PENDING' ? 'Joining...' : 'Join'}
           </button>
         </form>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default NewsletterForm
+export default NewsletterForm;
