4b7641daa47eb7fec89cfccb03c154f8219ba6da
Thomas <thomasfindlay94@gmail.com>
2022-07-15 12:21:17 +0100

    Added Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -11,9 +11,8 @@ module.exports = {
     require('postcss-extend'),
     require('postcss-mixins'),
     // Comment out postcss-nested if you're using tailwindcss/nesting
-    require('postcss-nested'),
-    // require('tailwindcss/nesting'),
-    // require('tailwindcss')('tailwind.config.js'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss')('tailwind.config.js'),
     require('postcss-preset-env', {
       autoprefixer: {
         flexbox: 'no-2009',
