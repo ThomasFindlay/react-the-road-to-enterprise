3af0acbb9166170bbea7b78c961bae07b617c158
Thomas <thomasfindlay94@gmail.com>
2022-07-15 11:15:42 +0100

    Final convert to Vite

--- src/components/accordion/accordion.spec.tsx
+++ src/components/accordion/accordion.spec.tsx
@@ -1,5 +1,5 @@
-import Accordion from './Accordion'
-import { render, fireEvent, screen } from 'test-utils'
+import Accordion from './Accordion';
+import { render, fireEvent, screen } from 'test-utils';
 
 const accordionData = [
   {
@@ -14,34 +14,34 @@ const accordionData = [
     heading: 'Three',
     content: 'Content Three',
   },
-]
+];
 
 const renderAccordion = (props = {}) =>
-  render(<Accordion items={accordionData} {...props} />)
+  render(<Accordion items={accordionData} {...props} />);
 
 describe('Accordion.tsx', () => {
   it('Accordion items have correct text', async () => {
-    renderAccordion()
+    renderAccordion();
     // Match Text content
     accordionData.forEach((data) => {
-      screen.getByText(data.heading)
-      screen.getByText(data.content)
-    })
-  })
+      screen.getByText(data.heading);
+      screen.getByText(data.content);
+    });
+  });
 
   it('Each accordion item content is hidden at the start', async () => {
-    renderAccordion()
+    renderAccordion();
     screen.getAllByTestId('accordion-item-content').forEach((el) => {
-      expect(el).toHaveClass('hidden')
-    })
-  })
+      expect(el).toHaveClass('hidden');
+    });
+  });
 
   it('Accordion item content is toggled on header click', async () => {
-    renderAccordion()
-    const heading = accordionData[1].heading
-    const content = accordionData[1].content
-    expect(screen.getByText(content)).toHaveClass('hidden')
-    fireEvent.click(screen.getByText(heading))
-    expect(screen.getByText(content)).toHaveClass('block')
-  })
-})
+    renderAccordion();
+    const heading = accordionData[1].heading;
+    const content = accordionData[1].content;
+    expect(screen.getByText(content)).toHaveClass('hidden');
+    fireEvent.click(screen.getByText(heading));
+    expect(screen.getByText(content)).toHaveClass('block');
+  });
+});
--- src/helpers/test-utils.tsx
+++ src/helpers/test-utils.tsx
@@ -1,15 +1,22 @@
-import { render, RenderOptions } from '@testing-library/react'
-import '@/index.css'
+import React from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import '@/index.css';
 
-const AllTheProviders: React.FC = ({ children }) => {
-  return <>{children}</>
-}
+export type AllTheProvidersProps<P> = P & {
+  children: React.ReactNode;
+};
+
+const AllTheProviders = <P extends Record<string, unknown>>({
+  children,
+}: AllTheProvidersProps<P>) => {
+  return <>{children}</>;
+};
 
 const customRender = (
   ui: React.ReactElement,
   options?: Omit<RenderOptions, 'queries'>
-) => render(ui, { wrapper: AllTheProviders, ...options })
+) => render(ui, { wrapper: AllTheProviders, ...options });
 
-export * from '@testing-library/react'
+export * from '@testing-library/react';
 
-export { customRender as render }
+export { customRender as render };
