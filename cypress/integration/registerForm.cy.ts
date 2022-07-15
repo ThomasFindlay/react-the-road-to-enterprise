592e2c059930efa7a8c21b014a1afc50335b2daf
Thomas <thomasfindlay94@gmail.com>
2022-07-15 13:53:35 +0100

    Renamed the test file

---
+++ cypress/integration/registerForm.cy.ts
@@ -0,0 +1,21 @@
+// Helper to go to the next step
+const goNext = () => cy.findByText('Next').click();
+
+type UserData = {
+  name: string;
+  surname: string;
+  address: string;
+  city: string;
+  email: string;
+  password: string;
+};
+
+describe('User Registration', () => {
+  beforeEach(() => {});
+
+  it('Visits the page', () => {});
+
+  it('Fill in the form', () => {});
+
+  it('Submit the form', () => {});
+});
