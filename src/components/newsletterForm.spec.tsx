01faa5577c8c1b0ae64ce501f5939461358d231d
Thomas <thomasfindlay94@gmail.com>
2022-07-15 12:08:56 +0100

    Cleanup and updated test-utils types

--- src/components/newsletterForm.spec.tsx
+++ src/components/newsletterForm.spec.tsx
@@ -1,24 +1,24 @@
-import NewsletterForm from './NewsletterForm'
-import { render } from 'test-utils'
+import NewsletterForm from './NewsletterForm';
+import { render } from 'test-utils';
 
 const renderNewsletterForm = () => {
-  const utils = render(<NewsletterForm />)
-  const nameInput = utils.getByLabelText('Name')
+  const utils = render(<NewsletterForm />);
+  const nameInput = utils.getByLabelText('Name');
   // const emailInput = utils.getByLabelText('Email')
-  const emailInput = utils.getByTestId('emailInput')
-  const submitBtn = utils.getByText('Join')
+  const emailInput = utils.getByTestId('emailInput');
+  const submitBtn = utils.getByText('Join');
   return {
     nameInput,
     emailInput,
     submitBtn,
     ...utils,
-  }
-}
+  };
+};
 
 describe('NewsletterForm.tsx', () => {
-  it('Should show an error message if submitting without filling in all the fields.', async () => {})
+  it('Should show an error message if submitting without filling in all the fields.', async () => {});
 
-  it('Should show a success message when the user joins the newsletter', async () => {})
+  it('Should show a success message when the user joins the newsletter', async () => {});
 
-  it('Should show an error if the request to join the newsletter fails.', async () => {})
-})
+  it('Should show an error if the request to join the newsletter fails.', async () => {});
+});
