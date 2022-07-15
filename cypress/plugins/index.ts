a71f521c19ba8525054d59707a29b47cb4c01ebf
Thomas <thomasfindlay94@gmail.com>
2022-07-15 13:42:55 +0100

    Convert setup

---
+++ cypress/plugins/index.ts
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+export default (
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+) => {
+  return Object.assign({}, config, {
+    fixturesFolder: 'cypress/fixtures',
+    integrationFolder: 'cypress/integration',
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
+    supportFile: 'cypress/support/index.ts',
+  });
+};
